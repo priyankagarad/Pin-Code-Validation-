9e5bebb660433bf2ae5386a9a304d6fb673a4fd2
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-11 21:16:00 +0530

    ADDED:Check PinCode is Valid or not

--- pinCodeValidation.sh
+++ pinCodeValidation.sh
@@ -3,6 +3,9 @@ echo "Welcome to Pin Code Validation"
 
 #constant
 PIN_CODE_IS_SIX_DIGIT="^[0-9]{6}$"
+PIN_CODE_PATTERN="^[0-9]{3}[ ]{1}[0-9]{3}$"
+
+#function check all pattern 
 function checkPattern()
 {
 	if [[ $1 =~ $2 ]]
@@ -13,9 +16,15 @@ function checkPattern()
 		echo "InValid"
 	fi
 }
-
-#check pin code contain 6 digit & restricted for alphabets and Numeric character At last position
+#check pin code contain 6 digit
+#check pin code do not Contain Alphabets and Numeric Character At First Position 
+#Check pin Code do not Contain alphabets and Numeric character At last position
 read -p "enter pin number:" pincode
 checkPattern $pincode $PIN_CODE_IS_SIX_DIGIT
+
+#check valid pin code pattern
+read -p "enter pin number:" pincode
+checkPattern $pincode $PIN_CODE_PATTERN
+
 
 
